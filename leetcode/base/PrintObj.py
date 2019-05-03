4862b863096ce3d1627c3c2617e94964f0a21271
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-05-03 21:05:34 +0800

    commit
    Change-Id: Ib63a2d8af5f34faf32b99a584ae0ed769018962a

--- leetcode/base/PrintObj.py
+++ leetcode/base/PrintObj.py
@@ -1,3 +1,6 @@
+import math
+
+from leetcode.base.Stack import Stack
 from leetcode.base.structure.ListNode import ListNode
 from leetcode.base.structure.TreeNode import TreeNode
 
@@ -56,8 +59,63 @@ def _printObjCore(obj, ext):
             print(ext, end='')
         print("")
     elif t == TreeNode:
-        pass
+        printTreeNode(obj)
+
+
+def printTreeNode(root):
+    if root is None:
+        return
+    globalStack = Stack()
+    globalStack.push(root)
+    depth = _getDepth(root)
+    nBlank = int(math.pow(2, depth + 1))
+    ndot = nBlank * 2
+    isRowEmpty = False
+    for i in range(ndot):
+        print('.', end='')
+    print("")
+    while not isRowEmpty:
+        localStack = Stack()
+        isRowEmpty = True
+        for i in range(nBlank):
+            print(' ', end='')
+        while not globalStack.isEmpty():
+            temp = globalStack.pop()
+            if temp is not None:
+                print(temp.val, end='')
+                print(' ', end='')
+                localStack.push(temp.left)
+                localStack.push(temp.right)
+                if temp.left is not None or temp.right is not None:
+                    isRowEmpty = False
+            else:
+                print('# ', end='')
+                localStack.push(None)
+                localStack.push(None)
+            for i in range(nBlank * 2 - 2):
+                print(' ', end='')
+        print("")
+        nBlank //= 2
+        while not localStack.isEmpty():
+            globalStack.push(localStack.pop())
+    for i in range(ndot):
+        print('.', end='')
+    print("")
+
+
+
+
+
+
+
+
+
+
+
 
 def _getDepth(root):
-    pass
-
+    if root is not None:
+        lDepth = _getDepth(root.left)
+        rDepth = _getDepth(root.right);
+        return (lDepth if lDepth > rDepth else rDepth) + 1
+    return 0
